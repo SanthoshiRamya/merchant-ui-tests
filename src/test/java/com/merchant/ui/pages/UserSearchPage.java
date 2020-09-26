9f0aab51357fc16cc5b20eeff0c48ac4295e3b27
swetha <swetha@py-home.attlocal.net>
2020-09-25 20:00:04 -0400

    Added null check

--- src/test/java/com/merchant/ui/pages/UserSearchPage.java
+++ src/test/java/com/merchant/ui/pages/UserSearchPage.java
@@ -14,81 +14,71 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.merchant.ui.TestBase;
 
-public class UserSearchPage extends TestBase{
-	
-	
+public class UserSearchPage extends TestBase {
+
 	private static final String RESULT_DATE_FORMAT = "MMMM dd, yyyy HH:mm";
 	private static final String SEARCG_DATE_FORMAT = "yyyy-MM-dd";
-	
-    protected WebDriverWait wait; 
- 
-    
-    @FindBy(xpath = "//*[@id=\"q_username_input\"]/select")
+
+	protected WebDriverWait wait;
+
+	@FindBy(xpath = "//*[@id=\"q_username_input\"]/select")
 	WebElement userNameSelect;
-	
-	
+
 	@FindBy(xpath = "//*[@id=\"q_username\"]")
 	WebElement userNameText;
-	
-	
-    @FindBy(xpath = "//*[@id=\"q_email_input\"]/select")
+
+	@FindBy(xpath = "//*[@id=\"q_email_input\"]/select")
 	WebElement emailSelect;
-	
-	
+
 	@FindBy(xpath = "//*[@id=\"q_email\"]")
 	WebElement emailText;
-	
-	
+
 	@FindBy(xpath = "//*[@id=\"new_q\"]/div[4]/input[1]")
 	WebElement filterButton;
-	
+
 	@FindBy(xpath = "//*[@id=\"index_table_users\"]")
 	WebElement userTable;
-	
-	
+
 	@FindBy(xpath = "//*[@id=\"new_q\"]/div[4]/a")
 	WebElement clearFilterButton;
-	
-	
+
 	@FindBy(xpath = "//*[@id=\"q_created_at_gteq_datetime\"]")
 	WebElement fromDate;
-	
-	@FindBy(xpath = "//*[@id=\"q_created_at_lteq_datetime\"]")
-	WebElement toDate;	
 
-	
+	@FindBy(xpath = "//*[@id=\"q_created_at_lteq_datetime\"]")
+	WebElement toDate;
+
 	public UserSearchPage() {
 		PageFactory.initElements(driver, this);
-        wait = new WebDriverWait(driver,20);
+		wait = new WebDriverWait(driver, 20);
 	}
-	
-	
+
 	public boolean searchByUserName(String selectValue, String searchText) {
-		
+
 		Select userSelect = new Select(userNameSelect);
 		userSelect.selectByValue(selectValue);
 		userNameText.sendKeys(searchText);
 		filterButton.submit();
-				
-        List<WebElement>  userNames = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[3]")); 
-        return searchCorrectUsersfound(userNames, searchText);
+
+		List<WebElement> userNames = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[3]"));
+		return searchCorrectUsersfound(userNames, searchText);
 	}
-	
+
 	public boolean searchByEmail(String selectValue, String searchText) {
-		
+
 		Select userSelect = new Select(emailSelect);
 		userSelect.selectByValue(selectValue);
 		emailText.sendKeys(searchText);
 		filterButton.submit();
-				
-        List<WebElement>  userNames = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[4]")); 
-        return searchCorrectUsersfound(userNames, searchText);
+
+		List<WebElement> userNames = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[4]"));
+		return searchCorrectUsersfound(userNames, searchText);
 	}
-	
+
 	public boolean searchByFromDate(String searchText) {
-		
-        SimpleDateFormat searchSdf = new SimpleDateFormat(SEARCG_DATE_FORMAT);
-        Date searchDate;
+
+		SimpleDateFormat searchSdf = new SimpleDateFormat(SEARCG_DATE_FORMAT);
+		Date searchDate;
 		try {
 			searchDate = searchSdf.parse(searchText);
 		} catch (ParseException e1) {
@@ -96,18 +86,19 @@ public class UserSearchPage extends TestBase{
 			e1.printStackTrace();
 			return false;
 		}
-		
+
 		fromDate.sendKeys(searchText);
 		filterButton.submit();
-				
-        List<WebElement>  datesSearchResult = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[5]")); 
-        boolean usernamesFound= datesSearchResult !=null && datesSearchResult.size() >0;
-        SimpleDateFormat resultSdf = new SimpleDateFormat(RESULT_DATE_FORMAT);
 
-        try {
-			for(WebElement element: datesSearchResult) {
-				Date date = resultSdf.parse(element.getText()) ;
-				if(! date.after(searchDate)) {
+		List<WebElement> datesSearchResult = driver
+				.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[5]"));
+		boolean usernamesFound = datesSearchResult != null && datesSearchResult.size() > 0;
+		SimpleDateFormat resultSdf = new SimpleDateFormat(RESULT_DATE_FORMAT);
+
+		try {
+			for (WebElement element : datesSearchResult) {
+				Date date = resultSdf.parse(element.getText());
+				if (!date.after(searchDate)) {
 					usernamesFound = false;
 					break;
 				}
@@ -115,16 +106,14 @@ public class UserSearchPage extends TestBase{
 		} catch (ParseException e) {
 			usernamesFound = false;
 		}
-       
-        return usernamesFound;
+
+		return usernamesFound;
 	}
-	
-	
-	
+
 	public boolean searchByToDate(String searchText) {
-		
-        SimpleDateFormat searchSdf = new SimpleDateFormat(SEARCG_DATE_FORMAT);
-        Date searchDate;
+
+		SimpleDateFormat searchSdf = new SimpleDateFormat(SEARCG_DATE_FORMAT);
+		Date searchDate;
 		try {
 			searchDate = searchSdf.parse(searchText);
 		} catch (ParseException e1) {
@@ -132,19 +121,19 @@ public class UserSearchPage extends TestBase{
 			return false;
 		}
 
-						
 		toDate.sendKeys(searchText);
 		filterButton.submit();
-				
-        List<WebElement>  datesSearchResult = driver.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[5]")); 
-        boolean usernamesFound= datesSearchResult !=null && datesSearchResult.size() >0;
-        SimpleDateFormat resultSdf = new SimpleDateFormat(RESULT_DATE_FORMAT);
-    
-        try {
-			for(WebElement element: datesSearchResult) {
-				Date date = resultSdf.parse(element.getText()) ;
-				if(! date.before(searchDate)) {
-					System.out.println(date +", "+searchDate);
+
+		List<WebElement> datesSearchResult = driver
+				.findElements(By.xpath("//*[@id=\"index_table_users\"]/tbody/tr/td[5]"));
+		boolean usernamesFound = datesSearchResult != null && datesSearchResult.size() > 0;
+		SimpleDateFormat resultSdf = new SimpleDateFormat(RESULT_DATE_FORMAT);
+
+		try {
+			for (WebElement element : datesSearchResult) {
+				Date date = resultSdf.parse(element.getText());
+				if (!date.before(searchDate)) {
+					System.out.println(date + ", " + searchDate);
 					usernamesFound = false;
 					break;
 				}
@@ -152,30 +141,26 @@ public class UserSearchPage extends TestBase{
 		} catch (ParseException e) {
 			usernamesFound = false;
 		}
-               
-        return usernamesFound;
+
+		return usernamesFound;
 	}
-	
-	
-	
+
 	public void clearFilters() {
-        clearFilterButton.click();
+		clearFilterButton.click();
 
 	}
-	
-	
-	
+
 	private boolean searchCorrectUsersfound(List<WebElement> columnDataList, String searchText) {
-        boolean usernamesFound= columnDataList !=null && columnDataList.size() >0;
-        
-        for(WebElement element: columnDataList) {
-        	if(!element.getText().contains(searchText)) {
-        		usernamesFound = false;
-        		break;
-        	}
-        }
-        
-        return usernamesFound;
+		boolean usernamesFound = columnDataList != null && columnDataList.size() > 0;
+		if (usernamesFound) {
+			for (WebElement element : columnDataList) {
+				if (!element.getText().contains(searchText)) {
+					usernamesFound = false;
+					break;
+				}
+			}
+		}
+		return usernamesFound;
 	}
 
 }
